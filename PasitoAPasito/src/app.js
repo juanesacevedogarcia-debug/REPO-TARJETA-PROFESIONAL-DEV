a6d62dda2be8a05873188643c57577f3433a9f63
juanesacevedogarcia-debug <juanes.acevedo.garcia@gmail.com>
2026-07-29 12:53:23 -0500

    TARJETA AVATAR

--- PasitoAPasito/src/app.js
+++ PasitoAPasito/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../Targeta-1.json')
+      require('../image-targets/Targeta-1.json')
     ],
   })
 }
